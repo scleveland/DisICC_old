f0599bed45de3327419e3364dc9504d844305339
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-01-31 16:36:44 -0700

    updating file destinations

--- app/models/alignment.rb
+++ app/models/alignment.rb
@@ -74,7 +74,7 @@ class Alignment
     alignments = Alignment.all(:alignment_name => self.alignment_name)
     alignments.each do |alignment|
       filename= alignment.generate_pid_fasta_file
-      string = "./lib/comp_apps/XDet/#{filename} Maxhom_McLachlan.metric >> #{filename+"_xdet"}"
+      string = "./lib/comp_apps/XDet/xdet_linux ../../../#{filename} Maxhom_McLachlan.metric >> ../../../#{filename}_xdet"
       if system(string)
         puts "hurray"
       end
